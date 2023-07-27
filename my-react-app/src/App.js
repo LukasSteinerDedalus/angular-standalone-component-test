e2123f890f914d4bbe6bcbc139bd03fe69e042e1
Harald Reingruber <harald.reingruber@dedalus.com>
2023-07-27 16:54:36 +0200

    Add react test app for standalone-component

---
+++ my-react-app/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import 'component-lib';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Angular standalone-custom-element test!
+        </p>
+        <form id="user-form">
+          <dbs-input label="Name" type="text" value="Hello world"></dbs-input>
+          <dbs-input label="Email" type="email" value="Hello world"></dbs-input>
+          <dbs-input label="Message" type="text" value="Hello world"></dbs-input>
+          <button onClick={getFormValues}>Submit</button>
+        </form>
+      </header>
+      
+    </div>
+  );
+}
+
+function getFormValues() {
+  const form = document.getElementById("user-form");
+
+  const data = Array.from(form.querySelectorAll("dbs-input")).map(
+    (input) => {
+      return {
+        label: input.label,
+        value: input.value,
+      };
+    }
+  );
+  console.log(data);
+}
+
+export default App;
